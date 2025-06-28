7ffc5968b466eb394d75b0855c687d181d4589e1
Denys Okunenko <dokunenko@cloverdynamics.com>
2025-06-28 03:50:46 +0300

    default settings

--- UkrChatSupport/Configuration.cs
+++ UkrChatSupport/Configuration.cs
@@ -2,15 +2,16 @@
 using Dalamud.Plugin;
 using System;
 
-namespace SamplePlugin;
+namespace UkrChatSupport;
 
 [Serializable]
 public class Configuration : IPluginConfiguration
 {
     public int Version { get; set; } = 0;
 
-    public bool IsConfigWindowMovable { get; set; } = true;
-    public bool SomePropertyToBeSavedAndWithADefault { get; set; } = true;
+    public bool ReactOnlyToUkLayout { get; set; } = true;
+    public bool ReplaceOnlyOnUkLayout { get; set; } = true;
+    public bool ReplaceInput { get; set; } = true;
 
     // the below exist just to make saving less cumbersome
     public void Save()
---
+++ UkrChatSupport/Windows/ConfigWindow.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Numerics;
+using Dalamud.Interface.Windowing;
+using ImGuiNET;
+
+namespace UkrChatSupport.Windows;
+
+public class ConfigWindow : Window, IDisposable
+{
+    private Configuration Configuration;
+
+    // We give this window a constant ID using ###
+    // This allows for labels being dynamic, like "{FPS Counter}fps###XYZ counter window",
+    // and the window ID will always be "###XYZ counter window" for ImGui
+    public ConfigWindow(Plugin plugin) : base("UkrChatSupport Configuration")
+    {
+        Flags = ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoScrollbar |
+                ImGuiWindowFlags.NoScrollWithMouse;
+
+        Size = new Vector2(320, 145);
+        SizeCondition = ImGuiCond.Always;
+
+        Configuration = plugin.Configuration;
+    }
+
+    public void Dispose() { }
+
+    public override void Draw()
+    {
+        var reactOnlyToUkLayout = Configuration.ReactOnlyToUkLayout;
+        if (ImGui.Checkbox("React only to ukrainian layout (chat window)", ref reactOnlyToUkLayout))
+        {
+            Configuration.ReactOnlyToUkLayout = reactOnlyToUkLayout;
+            Configuration.Save();
+        }
+
+        var replaceOnlyOnUkLayout = Configuration.ReplaceOnlyOnUkLayout;
+        if (ImGui.Checkbox("Replace only on ukrainian layout (input field)", ref replaceOnlyOnUkLayout))
+        {
+            Configuration.ReplaceOnlyOnUkLayout = replaceOnlyOnUkLayout;
+            Configuration.Save();
+        }
+
+        var replaceInput = Configuration.ReplaceInput;
+        if (ImGui.Checkbox("Replace keyboard input", ref replaceInput))
+        {
+            Configuration.ReplaceInput = replaceInput;
+            Configuration.Save();
+        }
+    }
+}
